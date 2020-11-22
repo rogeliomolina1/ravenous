836f31e8a28794a3b6d67a03b8a79daff8786f58
Rogelio Molina <rogelio.molina1@gmail.com>
2020-11-21 21:37:30 -0800

    almost ready for API

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,9 +3,9 @@ import './SearchBar.css';
 
 
 class SearchBar extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             term: '',
             location: '',
             sortBy: 'best_match'
@@ -14,29 +14,35 @@ class SearchBar extends React.Component {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
             'Most Reviewed': 'review_count'
-          };
-        this.handleTermChange=this.handleTermChange.bind(this);
-        this.handleLocationChange=this.handleLocationChange.bind(this);
+        };
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
-    getSortByClass(sortByOption){
-        if(sortByOption === this.state.sortBy){
+    getSortByClass(sortByOption) {
+        if (sortByOption === this.state.sortBy) {
             return 'active';
-        }else{
+        } else {
             return '';
         }
     }
 
-    handleSortByChange(sortByOption){
-
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy: sortByOption})
     }
 
-    handleTermChange(event){
-        this.setState({term: event.target.value});
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
     }
 
-    handleLocationChange(event){
-        this.setState({location: event.target.value});
+    handleLocationChange(event) {
+        this.setState({ location: event.target.value });
+    }
+
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
     }
 
     renderSortByOptions() {
@@ -58,7 +64,7 @@ class SearchBar extends React.Component {
                     <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         );
