c729d984aa7067709003a9bac9b625cbd7f010e7
Rogelio Molina <rogelio.molina1@gmail.com>
2020-11-22 00:08:46 -0800

    All done

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'lwb0hTAph_5PNl1aSyCOwNiLAX42UyyvE_mghwtuMGt-BIymXSvVMlvvzVXmSes09GcqO7bOV6psn0HuBeSJPZ-BevEgnwY7vI73GUJ5lUMsKkiLYqWQenlDy_y5X3Yx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            }
+        });
+    }
+}
+
+export default Yelp;
